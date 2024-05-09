fd1b9c4313681a90ac3cf2ad3ac94bef34f3049f
Nolan Berg <nolanb912@gmail.com>
2024-05-09 00:51:34 -0600

    update files

---
+++ code.js
@@ -0,0 +1,12 @@
+// Tail-recursive function for computing Fibonacci numbers
+function fibonacci(n) {
+    return fibonacciHelper(n, 0, 1);
+}
+
+function fibonacciHelper(n, a, b) {
+    if (n === 0) return a;
+    if (n === 1) return b;
+    return fibonacciHelper(n - 1, b, a + b);
+}
+
+module.exports = { fibonacci };
